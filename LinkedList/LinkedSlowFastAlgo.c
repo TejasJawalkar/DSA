4d803dc3ec1a574dd09d0b9ae4ae0f963cf01caf
Tejas Jawalkar <tejassj1998@gmail.com>
2025-11-14 21:53:05 +0530

    new chnag

---
+++ LinkedList/LinkedSlowFastAlgo.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+void insert_beg();
+void insert_cycle();
+
+struct Node
+{
+  struct Node *Next;
+  int Value;
+}*Start=NULL, *Curr=NULL, *Temp=NULL, *NewNode=NULL, *Next=NULL;
+
+
+int main()
+{
+  
+  return 0;
+}
